0a919dc89068326a754c73f9a685f3561e7e514d
damolaajayi <damolaajayi@gmail.com>
2021-04-15 23:34:03 +0100

    navbar styling

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,26 +1,33 @@
 <template>
   <nav
-    class="navbar fixed-top navbar-expand-sm navbar-light bg-light my-navbar"
+    class="navbar fixed-top navbar-expand-lg navbar-light bg-light my-navbar"
   >
     <div class="container">
       <router-link :to="{ name: 'Home' }" class="navbar-brand"
         >QuestionTime
       </router-link>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item">
             <router-link :to="{ name: 'Home' }" class="btn btn-sm btn-success"
               >Home
             </router-link>
-          </li>
-          <li class="nav-item mx-1">
             <router-link
               :to="{ name: 'question-editor' }"
-              class="btn btn-sm btn-danger"
+              class="btn btn-sm btn-danger mx-1"
               >Ask Question
             </router-link>
-          </li>
-          <li class="nav-item">
             <a class="btn btn-sm btn-outline-secondary" href="/accounts/logout/"
               >Logout</a
             >
@@ -46,7 +53,9 @@ export default {
   font-weight: bold;
   font-size: 130%;
 }
-
+.nav-link {
+  font-weight: bold;
+}
 .navbar-brand:hover {
   color: #dc3545 !important;
 }
